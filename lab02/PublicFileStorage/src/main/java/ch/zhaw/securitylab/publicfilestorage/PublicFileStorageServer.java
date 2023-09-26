f8cfae36b9e841f3272a3245da3a01f40f9d8841
Shazam <40243053+schazadm@users.noreply.github.com>
2023-09-26 20:27:27 +0200

    lab 02 done

--- lab02/PublicFileStorage/src/main/java/ch/zhaw/securitylab/publicfilestorage/PublicFileStorageServer.java
+++ lab02/PublicFileStorage/src/main/java/ch/zhaw/securitylab/publicfilestorage/PublicFileStorageServer.java
@@ -49,16 +49,34 @@ public class PublicFileStorageServer {
                 new InputStreamReader(socket.getInputStream()));
              OutputStreamWriter toClient = new OutputStreamWriter(socket.getOutputStream())) {
                 
-            // Read first line of request from the client
-            String line = fromClient.readLine();
+            // fromClient and toClient are used to read data from and write data to the client
+        try (BufferedReader fromClient = new BufferedReader(
+                new InputStreamReader(socket.getInputStream())); OutputStreamWriter toClient = new OutputStreamWriter(socket.getOutputStream())) {
+
+            // Read the first line of the request from the client
+            String line;
+            try {
+                line = readLineMaxChar(fromClient, 1000);
+            } catch (IOException e) {
+                writeNOKNoContent(toClient);
+                return;
+            }
+
             if (line == null) {
                 // Apparently, the client disconnected without sending anything, do nothing
             } else {
                 // System.out.println("First line of request: " + line);
+
+                // Validate the first line of the request
+                if (!validateFirstLineOfRequest(line)) {
+                    writeNOKNoContent(toClient);
+                    return;
+                }
+
                 // Get request type and argument from the first line of the request
                 int indexSpace = line.indexOf(' ');
                 String requestType = line.substring(0, indexSpace);
-                String argument = line.substring(indexSpace + 1);
+                String argument = urlDecode(line.substring(indexSpace + 1));
                 switch (requestType) {
                     case GET:
                         serveFile(toClient, argument);
@@ -82,15 +100,32 @@ public class PublicFileStorageServer {
     
     /* serveFile is used to return the content of a requested file */
     private void serveFile(OutputStreamWriter toClient, String filename) {
-            
+        if (!validateFilename(filename)) {
+            writeNOKNoContent(toClient);
+            return;
+        }
         // Read lines from file and send them to the client
-        String filepath = FILES_DIR + urlDecode(filename);
+        String filepath = FILES_DIR + filename;
         try (BufferedReader fromFile = new BufferedReader(new FileReader(filepath))) {
             writeOKContent(toClient);
-            String line = fromFile.readLine();
+
+            String line;
+            try {
+                line = readLineMaxChar(fromFile, 1000);
+            } catch (IOException e) {
+                writeNOKNoContent(toClient);
+                return;
+            }
+
             while (line != null) {
                 toClient.write(line + "\n");
-                line = fromFile.readLine();
+                // line = fromFile.readLine();
+                try {
+                    line = readLineMaxChar(fromFile, 1000);
+                } catch (IOException e) {
+                    writeNOKNoContent(toClient);
+                    return;
+                }
             }
             toClient.write(DONE + "\n");
         } catch (IOException e) {
@@ -103,14 +138,42 @@ public class PublicFileStorageServer {
             String filename) {
         try {
             // Read lines from client and write them to the specified file
-            fromClient.readLine(); // Absorb CONTENT control line
+            // fromClient.readLine(); // Absorb CONTENT control line
+            String line;
+            try {
+                line = readLineMaxChar(fromClient, 1000);
+            } catch (IOException e) {
+                writeNOKNoContent(toClient);
+                return;
+            }
+
             StringBuilder fileContent = new StringBuilder();
-            String line = fromClient.readLine();
+            //String line = fromClient.readLine();
+            try {
+                line = readLineMaxChar(fromClient, 1000);
+            } catch (IOException e) {
+                writeNOKNoContent(toClient);
+                return;
+            }
+            int linesAppended = 0;
             while ((line != null) && (!line.equals(DONE))) {
                 fileContent.append(line).append("\n");
-                line = fromClient.readLine();
+                linesAppended++;
+
+                if (linesAppended > 1000) {
+                    writeNOKNoContent(toClient);
+                    return;
+                }
+
+                // line = fromClient.readLine();
+                try {
+                    line = readLineMaxChar(fromClient, 1000);
+                } catch (IOException e) {
+                    writeNOKNoContent(toClient);
+                    return;
+                }
             }
-            String filepath = FILES_DIR + urlDecode(filename);
+            String filepath = FILES_DIR + filename;
             try (FileWriter toFile = new FileWriter(filepath)) {
                 toFile.write(fileContent.toString());
             }
@@ -122,9 +185,13 @@ public class PublicFileStorageServer {
         
     /* Execute a command and return results to client */
     private void executeSystemCommand(OutputStreamWriter toClient, String command) {
+        if (validateCommand(command) == false) {
+            writeNOKNoContent(toClient);
+            return;
+        }
         try {
             // Depending on the actual command, execute the right OS command
-            command = urlDecode(command);
+            //command = urlDecode(command);
             int indexSpace = command.indexOf(' ');
             String actualCommand = command.substring(0, indexSpace);
             String options = command.substring(indexSpace + 1);
@@ -184,6 +251,40 @@ public class PublicFileStorageServer {
             // Returns non-decoded input
         }
         return input;
+    }
+	
+	private boolean validateFirstLineOfRequest(String input) {
+        return input != null && input.matches("^(GET|PUT|SYSTEM) [\\x21-\\x7E][\\x20-\\x7E]*$");
+    }
+
+    private String readLineMaxChar(Reader reader, int max) throws IOException {
+        int count = 0;
+        int read;
+        String content = "";
+        while ((read = reader.read()) > 0) {
+            if (read == 0x0A) {
+                break;
+            }
+            count++;
+            if (count >= max) {
+                throw new IOException();
+            }
+            content += (char) read;
+
+        }
+
+        if (count == 0) {
+            return null;
+        }
+        return content;
+    }
+
+    private boolean validateFilename(String filename) {
+        return filename != null && filename.matches("[a-zA-Z0-9_+=$%?,.;:]{1,100}");
+    }
+
+    private boolean validateCommand(String command) {
+        return command != null && command.matches("USAGE [*.]");
     }
 
     /* main method */
